426eacbf941511bff17a92a42b10b6c9238352c2
Karina Pilusonoka <k.pilyushonoka@gmail.com>
2018-05-12 19:52:49 +0300

    Header sketch

--- front/src/app/components/header/header.component.ts
+++ front/src/app/components/header/header.component.ts
@@ -6,15 +6,15 @@ import { TrymeService } from '../tryme.service';
   template: `
   <div class="header">
     <a class="logo" href="#"><img src="assets/marcity-logo.png" /></a>
-    <div class="social">
+    <div class="social shadowed">
       <a class="social_icon" href="#"><img src="assets/soc-ico-fb.png" /></a>
       <a class="social_icon" href="#"><img src="assets/soc-ico-tw.png" /></a>
       <a class="social_icon" href="#"><img src="assets/soc-ico-gp.png" /></a>
     </div>
-    <nav class="navigation">
-      <a>base</a>
-      <a>active projevcts</a>
-      <a>our jury</a>
+    <nav class="navigation shadowed">
+      <a href="#">base</a>
+      <a href="#">active projevcts</a>
+      <a href="#">our jury</a>
     </nav>
   </div>
 `,
