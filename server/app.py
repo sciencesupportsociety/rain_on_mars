c3b9c66b1e0e97dac01e17f0b0615de3f792bd96
kriss <kriss.pujats@gmail.com>
2018-05-12 16:30:47 +0300

    stripe and flask

---
+++ server/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import stripe
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+stripe.api_key = config['stripe']['secret']
+
+app = Flask(__name__)
+
+
+@app.route('/invest', methods=['POST'])
+def charge():
+    data = request.json
+    amount = data['amount']
+    token = data['token']
+    try:
+        charged = stripe.Charge.create(
+            amount=amount,
+            currency="eur",
+            description="charge",
+            source=token,
+        )
+        return '', 204
+    except Exception as e:
+        return 'Could not process payment', 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
