4ea2e1e7c05a349e527c80ee61d9abe1965006b4
Karina Pilusonoka <k.pilyushonoka@gmail.com>
2018-05-12 15:09:05 +0300

    Credentials for stripe and firebase

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  stripePublishable: 'pk_test_1CeQcac0nu0fI09pdg5yyms7',
+  firebase: {
+    apiKey: 'AIzaSyAobLzoAPyh8w0w-tXGr14jp4CyJHRrez0',
+    authDomain: 'rain-on-mars.firebaseapp.com',
+    databaseURL: 'https://rain-on-mars.firebaseio.com',
+    projectId: 'rain-on-mars',
+    storageBucket: 'rain-on-mars.appspot.com',
+    messagingSenderId: '352420792528'
+  },
 };
 
 /*
