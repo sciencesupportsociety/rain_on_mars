f52f270910ddf4910d8a5c8086b0a8a7fe8d66db
Karina Pilusonoka <k.pilyushonoka@gmail.com>
2018-05-12 21:14:59 +0300

    Mars image

--- front/src/app/components/invest/invest.component.ts
+++ front/src/app/components/invest/invest.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   <div class="invest">
     <p class="your-money-i shadowed">100% of your money investments brings people to the Mars.</p>
     <p class="you-can-transform-th-copy-2 shadowed">You can transform the future of your children and yourself.</p>
+    <div class="mars"><img src="assets/Top-Mars.png" /></div>
   </div>
   `,
   styleUrls: ['./invest.component.css']
