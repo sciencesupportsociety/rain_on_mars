5d41d63e7b6d8464181a8618e8c9159ff9534f8d
kriss <kriss.pujats@gmail.com>
2018-05-13 00:44:51 +0300

    interactive map

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -4,12 +4,13 @@ import { AppStateService } from './shared/app-state.service';
 @Component({
   selector: 'app-root',
   template: `
+    <div class='container'>
     <app-header></app-header>
     <app-invest></app-invest>
     <app-map></app-map>
-
     <br><br>
     <app-stripe-form></app-stripe-form>
+    </div>
   `,
   styleUrls: ['./app.component.css']
 })
